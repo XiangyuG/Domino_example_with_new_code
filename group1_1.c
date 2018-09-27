9b60f2cadb65699dbcd617ebf06255e895cbbc88
XiangyuG <39160477+XiangyuG@users.noreply.github.com>
2018-09-27 00:11:31 -0400

    same_content_with_different_compilation_result
    The file with name fail is those that cannot get successful compilation

---
+++ group1_1.c
@@ -0,0 +1,28 @@
+#define FREEZE_TIME 10
+#define DELTA1 1
+#define DELTA2 2
+#define QMAX 5
+
+struct Packet {
+  int loss;
+  int qlen;
+  int now;
+  int link_idle;
+  int cond1;
+  int now_plus_free;
+};
+
+int last_update;
+int p_mark;
+
+void func(struct Packet p) {
+    p.now_plus_free = p.now - FREEZE_TIME;
+    p.qlen = p.now + DELTA1;
+    p.cond1 = p.now + QMAX;
+// Run when link goes idle (lower priority?)
+    if (p.now_plus_free > last_update) 
+	if (p.len>=1 && p.cond1>=5 && p.loss>=0){
+      p_mark = p_mark - DELTA2;
+      last_update = p.now;
+    }
+}
