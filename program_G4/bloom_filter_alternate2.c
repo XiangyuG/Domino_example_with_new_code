b0ee6e089bf890c8f85a511e9cfacdf00609ff8c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-10-10 17:55:01 -0400

    Another example (based on https://github.com/packet-transactions/domino-compiler/issues/44)

---
+++ program_G4/bloom_filter_alternate2.c
@@ -0,0 +1,40 @@
+struct Packet {
+  int sport;
+  int dport;
+  int member;
+  int filter1_idx;
+  int filter2_idx;
+  int filter3_idx;
+};
+
+int filter1[256];
+int filter2[256];
+int filter3[256];
+
+int hash1(int a, int b) {
+  return a * b;
+}
+
+int hash2(int a, int b) {
+  return a * b;
+}
+
+int hash3(int a, int b) {
+  return a * b;
+}
+
+void func(struct Packet pkt) {
+  pkt.filter1_idx = hash1(pkt.sport, pkt.dport) % 256;
+  pkt.filter2_idx = hash2(pkt.sport, pkt.dport) % 256;
+  pkt.filter3_idx = hash3(pkt.sport, pkt.dport) % 256;
+
+  if (filter1[pkt.filter1_idx] &&
+      filter2[pkt.filter2_idx] &&
+      filter3[pkt.filter3_idx]) {
+    pkt.member = 1;
+  }
+    filter1[pkt.filter1_idx] = 1;
+    filter2[pkt.filter2_idx] = 1;
+    filter3[pkt.filter3_idx] = 1;
+  
+}
